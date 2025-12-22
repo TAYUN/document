c9d6b643883fa63472cf9056d9a33471a676aba1
TAYUN <507028106@qq.com>
2025-12-22 22:32:32 +0800

    feat(validator): 添加中文验证消息和字段定义

---
+++ app/validators/zh/lang.ts
@@ -0,0 +1,8 @@
+export const validateMessage = {
+  minLength: '{{ field }} 至少需要 {{ min }} 个字符',
+  maxLength: '{{ field }} 不能超过 {{ max }} 个字符',
+}
+
+export const validateFields = {
+  title: '栏目名称',
+}
